a40f77c704e13cb1ad0f3d6809aeb3c894667935
Travis <travis.moulton002@gmail.com>
2021-04-08 19:39:13 -0400

    minor refactoring and commenting in create workout

--- src/containers/CreateWorkout/CreateWorkout.js
+++ src/containers/CreateWorkout/CreateWorkout.js
@@ -22,7 +22,7 @@ const CreateWorkout = (props) => {
   const { formData } = useSelector((state) => state.workout);
   const wgerDict = useSelector((state) => state.wgerDict);
   const dispatch = useDispatch();
-  const [favoritesAsExercies, setFavoritesAsExercies] = useState([]);
+  const [favoritesAsExercises, setFavoritesAsExercises] = useState([]);
   const [favoritesAsSelectOptions, setFavoritesAsSelectOptions] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [historyUsed, setHistoryUsed] = useState(false);
@@ -30,51 +30,6 @@ const CreateWorkout = (props) => {
   const [formIsValid, setFormIsValid] = useState(
     formData.workoutName ? true : false
   );
-
-  useEffect(() => {
-    if (props.history.location.state && !historyUsed) {
-      const { workout } = props.history.location.state;
-      dispatch(setExercises(workout.exercises));
-      dispatch(
-        setEntireForm(
-          workout.title,
-          workout.targetArea,
-          workout.secondaryTargetArea
-        )
-      );
-      setHistoryUsed(true);
-    }
-  }, [props.history.location.state, dispatch, historyUsed]);
-
-  useEffect(() => {
-    let arr = [];
-
-    (async () => {
-      if (favorites) {
-        arr = favorites.map((fav) =>
-          axios
-            .get(`https://wger.de/api/v2/exercise/${fav.exercise}`)
-            .then((res) => res.data)
-        );
-
-        if (!favoritesAsExercies.length)
-          setFavoritesAsExercies(await Promise.all(arr));
-      }
-    })();
-  }, [favoritesAsExercies, favorites]);
-
-  useEffect(() => {
-    if (favoritesAsExercies.length && !favoritesAsSelectOptions.length) {
-      setFavoritesAsSelectOptions(
-        favoritesAsExercies.map((exercise) => ({
-          value: exercise.id,
-          displayValue: exercise.name,
-        }))
-      );
-
-      setLoaded(true);
-    }
-  }, [favoritesAsExercies, favoritesAsSelectOptions]);
 
   const [workoutNameInput, setWorkoutNameInput] = useState({
     elementType: 'input',
@@ -156,6 +111,88 @@ const CreateWorkout = (props) => {
   });
 
   useEffect(() => {
+    let arr = [];
+
+    // Using the redux favorites array, make ajax calls to wger to get the exercise objects
+    // for each favorite.
+    (async () => {
+      if (favorites) {
+        const favoriteIds = favorites.map((favorite) => favorite.exercise);
+        console.log(favoriteIds.join(','));
+
+        arr = favorites.map((fav) =>
+          axios
+            .get(`https://wger.de/api/v2/exercise/${fav.exercise}`)
+            .then((res) => res.data)
+        );
+
+        if (!favoritesAsExercises.length)
+          setFavoritesAsExercises(await Promise.all(arr));
+      }
+    })();
+  }, [favoritesAsExercises, favorites]);
+
+  useEffect(() => {
+    // After favoritesAsExercises has been created, create an array of objects to
+    // be used as select options inside the Add from favroites dropdown
+    if (favoritesAsExercises.length && !favoritesAsSelectOptions.length) {
+      setFavoritesAsSelectOptions(
+        favoritesAsExercises.map((exercise) => ({
+          value: exercise.id,
+          displayValue: exercise.name,
+        }))
+      );
+      // Once the select options have been set, load the page
+      setLoaded(true);
+    }
+  }, [favoritesAsExercises, favoritesAsSelectOptions]);
+
+  useEffect(() => {
+    // If there are no favorites, the page can be loaded immediatley
+    if (favorites) if (!favorites.length && !loaded) setLoaded(true);
+  }, [favorites, loaded]);
+
+  // If taken to the create-workout component as a workout detail, load the
+  // exercises and populate the form from state.
+  useEffect(() => {
+    if (props.history.location.state && !historyUsed) {
+      const { workout } = props.history.location.state;
+
+      dispatch(setExercises(workout.exercises));
+      dispatch(
+        setEntireForm(
+          workout.title,
+          workout.targetArea,
+          workout.secondaryTargetArea
+        )
+      );
+
+      if (workout.title)
+        setWorkoutNameInput({ ...workoutNameInput, value: workout.title });
+
+      if (workout.targetArea)
+        setTargetAreaInput({
+          ...targetAreaInput,
+          value: workout.targetAreaCode,
+        });
+
+      if (workout.secondaryTargetArea)
+        setSecondaryTargetAreaInput({
+          ...secondaryTargetAreaInput,
+          value: workout.secondaryTargetCode,
+        });
+      setHistoryUsed(true);
+    }
+  }, [
+    props.history.location.state,
+    dispatch,
+    historyUsed,
+    targetAreaInput,
+    workoutNameInput,
+    secondaryTargetAreaInput,
+  ]);
+
+  useEffect(() => {
     if (
       favoritesAsSelectOptions.length &&
       !addFromFavorites.elementConfig.options.length
@@ -210,7 +247,7 @@ const CreateWorkout = (props) => {
   ));
 
   const addExerciseFromFavorites = (e) => {
-    const exercise = favoritesAsExercies.filter(
+    const exercise = favoritesAsExercises.filter(
       (fav) => fav.id === e.target.value * 1
     )[0];
 
@@ -247,7 +284,12 @@ const CreateWorkout = (props) => {
   };
 
   const clearWorkoutBtn = (
-    <button onClick={clearAllWorkoutData}>Clear form</button>
+    <button
+      style={{ display: 'block', margin: '.5rem auto' }}
+      onClick={clearAllWorkoutData}
+    >
+      Clear form
+    </button>
   );
 
   const finalDisplay = (
